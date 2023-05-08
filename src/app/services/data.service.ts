9c168a4044d356b6a14196b06b0d7c537710b66a
Luciano Rodríguez <luckidetrenque@gmail.com>
2023-05-08 20:48:28 -0300

    Cambios en responseType de servicios

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -12,22 +12,22 @@ export class DataService {
   constructor(private httpClient: HttpClient) {}
 
   public getOneData<T>(path: string, id: number): Observable<T> {
-    return this.httpClient.get<T>(`${this.apiUrl}${path}/${id}`, {responseType: 'json'});
+    return this.httpClient.get<T>(`${this.apiUrl}${path}/${id}`, {responseType: 'text' as 'json'});
   }
 
   public getData<T>(path: string): Observable<T> {
-    return this.httpClient.get<T>(`${this.apiUrl}${path}`, {responseType: 'json'});
+    return this.httpClient.get<T>(`${this.apiUrl}${path}`, {responseType: 'text' as 'json'});
   }
 
   public createData<T>(path: string, body: T): Observable<T> {
-    return this.httpClient.post<T>(`${this.apiUrl}${path}`, body);
+    return this.httpClient.post<T>(`${this.apiUrl}${path}`, body, {responseType: 'text' as 'json'});
   }
 
   public deleteData(path: string, id: number): Observable<void> {
-    return this.httpClient.delete<void>(`${this.apiUrl}${path}/${id}`);
+    return this.httpClient.delete<void>(`${this.apiUrl}${path}/${id}`, {responseType: 'text' as 'json'});
   }
 
   public updateData<T>(path: string, id: number, body: T): Observable<T> {
-    return this.httpClient.put<T>(`${this.apiUrl}${path}/${id}`, body);
+    return this.httpClient.put<T>(`${this.apiUrl}${path}/${id}`, body, {responseType: 'text' as 'json'});
   }
 }
