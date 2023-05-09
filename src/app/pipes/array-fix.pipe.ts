9f7574f39bfc2445a5aae71f2a9b5888eb618508
Luciano Rodríguez <luckidetrenque@gmail.com>
2023-05-09 13:07:36 -0300

    Se agrega el pipe ArrayFix

---
+++ src/app/pipes/array-fix.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'arrayFix',
+  pure: true
+})
+
+export class ArrayFixPipe implements PipeTransform {
+
+  transform(objectOrArray: any): any[] {
+    if (!objectOrArray) {
+      return [];
+    } else {
+      return this.ArrayFix(objectOrArray);
+    }
+  }
+
+  ArrayFix(objectOrArray: any): any {
+    return Array.isArray(objectOrArray) ? objectOrArray : [objectOrArray];
+  }
+
+}
