2e33bf59ce5abb11dc2868037457b6f8464338b8
Luciano Rodríguez <luckidetrenque@gmail.com>
2023-05-08 22:03:19 -0300

    Cambio en responseType

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,7 +1,15 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
+
+const HTTPOptions: Object = {
+
+  headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+  }),
+  responseType: 'text'
+}
 
 @Injectable({
   providedIn: 'root'
@@ -12,22 +20,22 @@ export class DataService {
   constructor(private httpClient: HttpClient) {}
 
   public getOneData<T>(path: string, id: number): Observable<T> {
-    return this.httpClient.get<T>(`${this.apiUrl}${path}/${id}`, {responseType: 'text' as 'json'});
+    return this.httpClient.get<T>(`${this.apiUrl}${path}/${id}`, HTTPOptions);
   }
 
   public getData<T>(path: string): Observable<T> {
-    return this.httpClient.get<T>(`${this.apiUrl}${path}`, {responseType: 'text' as 'json'});
+    return this.httpClient.get<T>(`${this.apiUrl}${path}`, HTTPOptions);
   }
 
   public createData<T>(path: string, body: T): Observable<T> {
-    return this.httpClient.post<T>(`${this.apiUrl}${path}`, body, {responseType: 'text' as 'json'});
+    return this.httpClient.post<T>(`${this.apiUrl}${path}`, body, HTTPOptions);
   }
 
   public deleteData(path: string, id: number): Observable<void> {
-    return this.httpClient.delete<void>(`${this.apiUrl}${path}/${id}`, {responseType: 'text' as 'json'});
+    return this.httpClient.delete<void>(`${this.apiUrl}${path}/${id}`, HTTPOptions);
   }
 
   public updateData<T>(path: string, id: number, body: T): Observable<T> {
-    return this.httpClient.put<T>(`${this.apiUrl}${path}/${id}`, body, {responseType: 'text' as 'json'});
+    return this.httpClient.put<T>(`${this.apiUrl}${path}/${id}`, body, HTTPOptions);
   }
 }
